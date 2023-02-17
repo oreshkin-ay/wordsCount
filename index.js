11761309749297ab86e9f8f8c769f9c57b0c2d00
a.oreshkin <a.oreshkin@corp.mail.ru>
2023-02-17 15:39:45 +0300

    ex1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world');
+const utils = require('./utils');
+
+
+utils.readLine('Enter text: ')
+    .then(result => {
+        const words = result.match(/[a-zA-Z]+/g);
+
+        console.log(`Number of words: ${words?.length ?? 0}`)
+    })
